2dbf3c9641a34dc1f767aa3769b993368a948b93
Marist User <Marist User>
2016-12-01 20:15:44 -0500

    Puts ratio of plays/likes in author name

--- content.js
+++ content.js
@@ -3,9 +3,6 @@ window.onload = function() {
 
   let songs = document.getElementsByClassName('soundList__item');
   console.info("Hey I'm actually getting called.");
-  console.info(songs);
-  console.info(songs.length);
-  console.info(songs[0]);
   for (var i = 0; i < songs.length; i++) {
 
     let currentSong = songs[i];
@@ -16,12 +13,12 @@ window.onload = function() {
     let likes = parseInt(currentSong.getElementsByClassName('sc-button-like')[0].innerHTML);
     var plays = currentSong.getElementsByClassName('sc-visuallyhidden')[1].innerHTML; // second visually hidden element
 
+    // plays comes like "12,445 plays to date" or something like that
     plays = plays.replace(/\D/g, ''); // remove all non digit characters
-    console.info(parseInt(plays));
 
-    let ratio = parseInt(plays / likes); // simple ratio for now
+    let ratio = parseInt(parseInt(plays) / likes); // simple ratio for now
     console.info("Ratio: " + ratio);
 
-    songs
+    currentSong.getElementsByClassName("soundTitle__usernameText")[0].innerHTML += " RATIO: <strong>" + ratio + "</strong>";
   }
 }
